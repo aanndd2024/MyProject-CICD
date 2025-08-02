07a84739a00ad6b44deaa39169f2e9e2d2912e28
Anand Yadav <aanand.18jan@gmail.com>
2025-08-02 13:16:37 -0400

    Initial Commit

---
+++ SchoolDemoCICD/SchoolDemoCICD/SchoolDemoCICDApp.swift
@@ -0,0 +1,17 @@
+//
+//  SchoolDemoCICDApp.swift
+//  SchoolDemoCICD
+//
+//  Created by Anand Yadav on 02/08/25.
+//
+
+import SwiftUI
+
+@main
+struct SchoolDemoCICDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SchoolDemoCICD/SchoolDemoCICDTests/SchoolDemoCICDTests.swift
@@ -0,0 +1,17 @@
+//
+//  SchoolDemoCICDTests.swift
+//  SchoolDemoCICDTests
+//
+//  Created by Anand Yadav on 02/08/25.
+//
+
+import Testing
+@testable import SchoolDemoCICD
+
+struct SchoolDemoCICDTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
